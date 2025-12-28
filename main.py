44b06f6a0839ed8dae4c1240ea44a8124164871a
dyldug <dylan.dugadko@gmail.com>
2025-12-28 23:08:23 +0200

    added a few basic htmls, started to work with base and with css styles

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
     return render_template("index.html")
 
 @app.route('/contact')
-def hello():
-    return render_template("index.html")
+def contact():
+    return render_template("contact.html")
+
+@app.route('/gallery')
+def gallery():
+    return render_template("gallery.html")
+
+@app.route('/base')
+def base():
+    return render_template("base.html")
